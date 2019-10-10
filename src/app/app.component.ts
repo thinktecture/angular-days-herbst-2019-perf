9f49ecc82471a738387e188af34920ddbe07bcff
Christian Liebel <christian.liebel@thinktecture.com>
2019-10-10 14:31:36 +0200

    Ex. 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'perf-demo';
+export class AppComponent implements OnInit {
+  get title() { console.log('CD'); return 'title'; }
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      requestAnimationFrame(() => this.ngOnInit());
+    });
+  }
 }
