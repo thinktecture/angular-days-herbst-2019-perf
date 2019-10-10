293c5e037280abb726f98246ff74c697fd80b561
Christian Liebel <christian.liebel@thinktecture.com>
2019-10-10 16:05:01 +0200

    Ex. 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'lazy',
+  loadChildren: () => import('./lazy/lazy.module')
+    .then(l => l.LazyModule)
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
